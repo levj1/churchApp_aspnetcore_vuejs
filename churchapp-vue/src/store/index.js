36b4f6abad34ab76de15d45f910e61045c1b22fb
levj1 <levj1@yahoo.com>
2020-02-18 16:38:49 -0500

    axios and http call

--- churchapp-vue/src/store/index.js
+++ churchapp-vue/src/store/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    givers: [],
   },
   mutations: {
+    updateGivers(state, givers){
+      state.givers = givers;
+    }
   },
   actions: {
+    getGivers( {commit} ){
+      axios.get('/api/persons')
+      .then(result => commit('updateGivers', result.data))
+      .catch(console.error);
+    }
   },
   modules: {
   }
--- churchapp-vue/src/views/giver/Giver.vue
+++ churchapp-vue/src/views/giver/Giver.vue
@@ -3,3 +3,22 @@
         <h1>Givers</h1>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'giver',
+    created(){
+        this.$store.dispatch('getGivers');
+    },
+    computed:{
+        // givers(){
+        //     return this.$store.state.givers;
+        // }
+    },
+    data(){
+        return{
+            count: 0,
+        }
+    }
+}
+</script>
